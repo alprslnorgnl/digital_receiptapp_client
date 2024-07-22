5a97288bd69949ae019674a61cc3adffe4180efa
idris <alparslanorgnl@gmail.com>
2024-07-22 20:46:20 +0300

    colors.dart updated

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+// FONT COLORS
+String fontColor1 = "000000"; //Black
+String fontColor2 = "FFFFFF"; //White
+String fontColor3 = "827777"; //Gray
+String fontColor4 = "2456E1"; //Blue
+String fontColor5 = "FF0000"; //Red
+
+// BUTTON COLORS
+String buttonColor1 = "33B338"; //Green
+String buttonColor2 = "99582A"; //Brown
+
+// Receipt Colors
+String receiptColor = "B8C0E8";
