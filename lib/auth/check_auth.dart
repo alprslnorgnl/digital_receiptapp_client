dc177383a2bbdc37d0490ff19cb4044e11bcc59b
idris <alparslanorgnl@gmail.com>
2024-07-22 20:02:44 +0300

    check_auth code written

---
+++ lib/auth/check_auth.dart
@@ -0,0 +1,48 @@
+//Buradaki işlem basitçe şudur: widget ilk olarak çağrıldığında
+//mevcutta bir tokenin kayıtlı olup olmadığı kontrol edilir.
+//eğer değilse --> Welcome sayfasına
+//eğer kayıtlıysa --> CheckReceiptPiece sayfasına yönlendirilir
+
+import 'package:flutter/material.dart';
+import 'package:receipt_app/view/components/checkReceiptPiece.dart';
+import 'package:receipt_app/view/screens/welcome.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CheckAuth extends StatefulWidget {
+  const CheckAuth({super.key});
+
+  @override
+  State<CheckAuth> createState() => _CheckAuthState();
+}
+
+class _CheckAuthState extends State<CheckAuth> {
+  @override
+  void initState() {
+    super.initState();
+    _checkToken();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  Future<void> _checkToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString('jwt_token') ?? '';
+
+    if (token.isEmpty) {
+      print('Oturum açılmamış, Welcome sayfasına yönlendiriliyor...');
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => const Welcome()));
+    } else {
+      print('Oturum Tokeni: $token');
+      Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => const CheckReceiptPiece()));
+    }
+  }
+}
